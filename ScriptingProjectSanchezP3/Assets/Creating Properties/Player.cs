09dbc39601027c7241f5247276765a4b43a821ac
Adam Sanchez <145504826+sghetwet@users.noreply.github.com>
2025-02-04 10:35:01 -0800

    fix like one thing
    a

---
+++ ScriptingProjectSanchezP3/Assets/Creating Properties/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public class Player : MonoBehaviour
+{
+
+    private int experience;
+    // Start is called before the first frame update
+    public int Experience
+    {
+        get
+        {
+            return experience;
+        }
+        set
+        {
+            experience = value; 
+        }
+    }
+
+    // Update is called once per frame
+    public int Level
+    {
+        get
+        {
+            return experience / 1000;
+        }
+        set
+        {
+            experience = value * 1000;
+        }
+    }
+
+    public int Health { get; set; }
+}
