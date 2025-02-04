6ec03b6596413242acb7c9a66b797f6048b37213
Adam Sanchez <145504826+sghetwet@users.noreply.github.com>
2025-02-04 10:37:00 -0800

    finsiehd first
    a

--- ScriptingProjectSanchezP3/Assets/Creating Properties/Player.cs
+++ ScriptingProjectSanchezP3/Assets/Creating Properties/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 
-public class Player : MonoBehaviour
+public class Player
 {
 
     private int experience;
