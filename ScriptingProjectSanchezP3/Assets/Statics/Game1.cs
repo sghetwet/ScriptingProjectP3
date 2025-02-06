9fa03d9cd94698067c02fac30fc5112877be23a4
Adam Sanchez <145504826+sghetwet@users.noreply.github.com>
2025-02-06 10:35:15 -0800

    a
    a

--- ScriptingProjectSanchezP3/Assets/Statics/Game1.cs
+++ ScriptingProjectSanchezP3/Assets/Statics/Game1.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1 : MonoBehaviour
+public class Game1 : MonoBehaviour
 {
-
-    public static int playerCount = 0;
     // Start is called before the first frame update
     void Start()
     {
-        playerCount++;
+        Enemy enemy1 = new Enemy();
+        Enemy enemy2 = new Enemy();
+        Enemy enemy3 = new Enemy();
+
+        int x = Enemy.enemyCount;
     }
 
     // Update is called once per frame
--- ScriptingProjectSanchezP3/Assets/Statics/Player1.cs
+++ ScriptingProjectSanchezP3/Assets/Statics/Player1.cs
@@ -2,16 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour
+public class Player : MonoBehaviour
 {
+
+    public static int playerCount = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Enemy enemy1 = new Enemy();
-        Enemy enemy2 = new Enemy();
-        Enemy enemy3 = new Enemy();
-
-        int x = Enemy.enemyCount;
+        playerCount++;
     }
 
     // Update is called once per frame
